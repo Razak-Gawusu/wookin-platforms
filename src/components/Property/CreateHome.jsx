fe6a354b1df64cbd41c2a0709d9278aa7298ca29
Gawusu Razak <gawusuLarbil@gmail.com>
2023-08-15 17:05:50 +0000

    initail commit

--- src/components/Property/CreateHome.jsx
+++ src/components/Property/CreateHome.jsx
@@ -9,7 +9,25 @@ export function CreateHome() {
 
   const { agents } = useAgents();
   const { properties } = useProperties();
-  const {} = useCreateHome(image, agent, created_at);
+
+  const onHandleSubmit = () => {
+    fetch("https://ownkey-real-estate-main-3113cd8.d2.zuplo.dev/properties", {
+      method: "POST",
+
+      body: JSON.stringify({
+        image,
+        agent,
+        created_at,
+      }),
+
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+        Authorization: `Bearer ${import.meta.env.VITE_WOOKIN_KEY}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+  };
 
   function handleSubmit(e) {
     e.preventDefault();
